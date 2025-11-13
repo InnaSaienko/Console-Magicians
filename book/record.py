21f4cb0fcf1d24fc918b921b0b4adec59140a433
Kateryna <k.churuta@gmail.com>
2025-11-13 20:34:25 +0000

    Update Record / str ,add_phone,add_email, add_address, find_phone, remove_phone, edit_phone

--- book/record.py
+++ book/record.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
-from fields_type import Name, Phone, Birthday, Email, Note
+from fields_type import Name, Phone, Birthday, Email, Note, Address
 
 
 @dataclass
@@ -13,38 +13,61 @@ class Record:
 
     def __str__(self):
         phone_str = '; '.join(p.value for p in self.phones)
-        return f"Contact name: {self.name.value}, birthday: {self.birthday}, phones: {phone_str}"
+        email_str = '; '.join(e.value for e in self.emails) if self.emails else "N/A"
+        address_str = f", Address: {self.address.value}" if self.address else ""
+        birthday_str = f", Birthday: {self.birthday}" if self.birthday else ""
+        return (
+            f"Contact name: {self.name.value}"
+            f"{birthday_str}" 
+            f"{address_str}"
+            f", Email: {email_str}"
+            f", Phone: {phone_str}"
+        )
 
     def add_phone(self, phone: str) -> bool:
-        pass
+        try:
+            self.phones.append(Phone(phone))
+            return True
+        except ValueError:
+            return False
 
     def add_email(self, email: str) -> bool:
-       pass
+        try:
+            self.emails.append(Email(email))
+            return True
+        except ValueError:
+            return False
 
     def add_birthday(self, birthday) -> bool:
-        # some code
-        return True
-
-    def add_note(self, note: Note) -> bool:
-        # some code
-        return True
-
-    def find_notes_by_tag(self, tag: str) -> list[Note]:
-        # use return comprehension for iterate through a list of notes for return all matched by tag notes
-        pass
-
-
+        try:
+            self.birthday = Birthday(birthday)
+            return True
+        except ValueError:
+            return False
+        
+    def add_address(self, address_str: str) -> bool:
+            self.address = Address(address_str)
+            return True    
+    
+        
     def find_phone(self, phone: str) -> Optional[Phone]:
-        # return phone for phone in phones if phone.value == phone
-        # next(phone,  None)
-        pass
+        return next((p for p in self.phones if p.value == phone), None)
 
     def remove_phone(self, phone: str) -> bool:
-        # return True if self.find_phone(phone) else return False
-        pass
+        phone_to_remove = self.find_phone(phone)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+            return True
+        return False
 
     def edit_phone(self, old_phone: str, new_phone: str) -> bool:
-       # return True if phone was updated else False
-        pass
-
-
+        phone = self.find_phone(old_phone)
+        if phone:
+            try:
+                validated_new_phone = Phone(new_phone) 
+                index = self.phones.index(phone)
+                self.phones[index] = validated_new_phone
+                return True
+            except ValueError:
+                return False
+        return False   
