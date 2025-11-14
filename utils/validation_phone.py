6594c73c3c0d86683f86bfce11d953eb5561d8c6
hontonoran <hontonoran@gmail.com>
2025-11-14 15:32:36 +0200

    add informative error messages to phone validation

--- utils/validation_phone.py
+++ utils/validation_phone.py
@@ -5,10 +5,10 @@ def validation_phone(phone: str) -> str | ValueError:
     try:
         phone_number_obj = phonenumbers.parse(phone, None)
         if not phonenumbers.is_valid_number(phone_number_obj):
-            raise ValueError
+            raise ValueError(f"Invalid phone number '{phone}'. Must be a valid number.")
         formatted_phone_number = phonenumbers.format_number(
             phone_number_obj, phonenumbers.PhoneNumberFormat.E164
         )
         return formatted_phone_number
     except NumberParseException:
-        raise ValueError
+        raise ValueError(f"Invalid phone number '{phone}'. Unable to parse.")
