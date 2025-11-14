e42fa1d33ced7a261068583a1c67460f61870359
hontonoran <hontonoran@gmail.com>
2025-11-14 11:43:56 +0200

    enrich phone number validation with phone numbers lib

--- utils/validation_phone.py
+++ utils/validation_phone.py
@@ -1,5 +1,14 @@
-import re
+import phonenumbers
+from phonenumbers.phonenumberutil import NumberParseException
 
 def validation_phone(phone: str) -> str | ValueError:
-    formatted_phone_number = re.sub(r"[^\d+]", "", phone)
-    return formatted_phone_number
+    try:
+        phone_number_obj = phonenumbers.parse(phone, None)
+        if not phonenumbers.is_valid_number(phone_number_obj):
+            raise ValueError
+        formatted_phone_number = phonenumbers.format_number(
+            phone_number_obj, phonenumbers.PhoneNumberFormat.E164
+        )
+        return formatted_phone_number
+    except NumberParseException:
+        raise ValueError
